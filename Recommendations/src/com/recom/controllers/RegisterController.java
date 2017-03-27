2847d1ed05140569435302797234855920915295
pkj <pkj@192.168.0.9>
2017-03-26 22:21:27 -0400

    Database connectivity.

--- Recommendations/src/com/recom/controllers/RegisterController.java
+++ Recommendations/src/com/recom/controllers/RegisterController.java
@@ -1,6 +1,7 @@
 package com.recom.controllers;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,12 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 public class RegisterController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterController() {
-        super();
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
--- Recommendations/src/com/recom/utils/DatabaseUtil.java
+++ Recommendations/src/com/recom/utils/DatabaseUtil.java
@@ -1,5 +1,62 @@
 package com.recom.utils;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.mysql.jdbc.Driver;
+
 public class DatabaseUtil {
 
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+		if (connection != null)
+			return connection;
+		else {
+			try {
+				
+				Properties prop = new Properties();
+				InputStream inputStream = DatabaseUtil.class.getClassLoader()
+						.getResourceAsStream("/db.properties");
+				prop.load(inputStream);
+				
+				String driver = prop.getProperty("driver");
+				String url = prop.getProperty("url");
+				String user = prop.getProperty("user");
+				String password = prop.getProperty("password");
+				
+				System.out.println("DRIVER NAME " + driver);
+				
+				// Load the JDBC driver
+				Class driver_class = Class.forName(driver);
+				Driver driver1;
+				
+				try {
+					driver1 = (Driver) driver_class.newInstance();
+					DriverManager.registerDriver(driver1);
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+				
+				connection = DriverManager.getConnection(url, user, password);
+				System.out.println("\n CONNECTION " + connection.toString());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return connection;
+		}
+	}	
 }
